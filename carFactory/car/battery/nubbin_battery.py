8ddeb1c7fe33d10f435db235daac6fab30ff2ae3
WenlingLyu <wlyu1@nait.ca>
2023-08-09 12:24:13 -0600

    working with it

---
+++ carFactory/car/battery/nubbin_battery.py
@@ -0,0 +1,12 @@
+from abc import ABC
+from datetime import datetime, timedelta
+from car import Car
+
+
+class NubbinBattery(Car, ABC):
+    def __init__(self, last_service_date):
+        self.current_date = datetime.now().date()
+        self.last_service_mileage = last_service_mileage
+
+    def battery_should_be_serviced(self):
+        return self.current_date - timedelta(days=730) >= self.last_service_date 
